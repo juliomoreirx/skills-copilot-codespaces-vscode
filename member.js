db6b6e12523a135a11b51e6c376da73941384060
Julio Moreira <julinhopentakill@gmail.com>
2025-05-22 12:33:41 +0000

    add member.js

---
+++ member.js
@@ -0,0 +1,60 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  );
+}
+function projectsMember() {
+  return (
+    <div className="projects-member">
+      <h2>Projects</h2>
+      <ul>
+        <li>Project A</li>
+        <li>Project B</li>
+        <li>Project C</li>
+        <li>Project D</li>
+        <li>Project E</li>
+      </ul>
+    </div>
+  );
+}
+function experienceMember() {
+  return (
+    <div className="experience-member">
+      <h2>Experience</h2>
+      <ul>
+        <li>Company A - Role A</li>
+        <li>Company B - Role B</li>
+        <li>Company C - Role C</li>
+        <li>Company D - Role D</li>
+        <li>Company E - Role E</li>
+      </ul>
+    </div>
+  );
+}
+function educationMember() {
+  return (
+    <div className="education-member">
+      <h2>Education</h2>
+      <ul>
+        <li>University A - Degree A</li>
+        <li>University B - Degree B</li>
+        <li>University C - Degree C</li>
+        <li>University D - Degree D</li>
+        <li>University E - Degree E</li>
+      </ul>
+    </div>
+  );
+}
+
+
+
+
